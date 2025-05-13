adba91c871c947c6c9820cd66beb0b90b3dc55a7
MaxiCode <andresholsbach@gmail.com>
2025-05-13 15:26:28 -0300

    Se agregan modificaciones

--- apuntesParcial1/ejercicios/ejercicio6.py
+++ apuntesParcial1/ejercicios/ejercicio6.py
@@ -11,12 +11,12 @@ for i in range(N):
 contador = 0
 
 for i in range(N):
-    if A[0] < A[i] and A[0] < B[i]:
+    if A[0] == A[i] and A[0] == B[i]:
         contador += 1
         N = N - contador
 resultado = str(contador)
 
-if A[0] < 1:
+if A[0] == 1:
     resultado = "verdadero"
 elif A[0] == 2:
     resultado = "2"
